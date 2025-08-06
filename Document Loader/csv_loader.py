7df8ec9ca9fd6b8b92d17d19d95061a389a9366a
Abhishek Badwar <98725240+Abhibadwar@users.noreply.github.com>
2025-08-06 10:52:26 +0530

    Delete Document Loader directory

--- Document Loader/csv_loader.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.document_loaders import CSVLoader
-
-
-loader = CSVLoader(file_path="RAG\Document Loader\Social_Network_Ads.csv", encoding="utf-8")
-
-documents = loader.load()
-
-print(len(documents))
-print(documents[1])
